922e0f6cb394b949f43dc0000801d1fdf28bee79
Анастасия <morina.anastasiya@gmail.com>
2014-10-22 22:55:58 +0400

    Update AddressManager.java

--- src/AddressManager.java
+++ src/AddressManager.java
@@ -10,10 +10,11 @@ public class AddressManager {
             System.out.println("Доступные операции:");
             System.out.println("1. Добавить запись в книгу");
             System.out.println("2. Поиск записи по имени человека");
-            System.out.println("3. Вывод всех записей на экран");
-            System.out.println("4. Удаление записи из книги по индексу");
-            System.out.println("5. Изменение записи");
-            System.out.println("6. Выход");
+            System.out.println("3. Поиск записи по индексу записи в книге");
+            System.out.println("4. Вывод всех записей на экран");
+            System.out.println("5. Удаление записи из книги по индексу");
+            System.out.println("6. Изменение записи");
+            System.out.println("7. Выход");
             System.out.println("Введите нужный пункт меню: ");
             Scanner sc = new Scanner(System.in);
             String menu = sc.nextLine();
@@ -22,59 +23,69 @@ public class AddressManager {
                 case 1: {
                     System.out.println("Введите имя:");
                     String name = sc.nextLine();
-                    System.out.println("Введите телефон:");
+                    System.out.println("Введите телефон (в случае отстутствия введите \"нет\"):");
                     String phone = sc.nextLine();
-                    System.out.println("Введите емайл:");
+                    System.out.println("Введите емайл (в случае отстутствия введите \"нет\"):");
                     String email = sc.nextLine();
-                    AddressBook.add(addressBook, name, phone, email);
-                    System.out.println("Запись успешно добавлена");
+                    System.out.println("Введите год рождения:");
+                    String year = sc.nextLine();
+                    AddressBook.add(addressBook, name, phone, email, year);
+                    System.out.println("Запись успешно добавлена!");
                     break;
                 }
                 case 2: {
-                    System.out.println("Введите имя:");
+                    System.out.println("Введите имя для поиска:");
                     String name = sc.nextLine();
                     int [] poisk = AddressBook.find(addressBook, name);
                     if (poisk.length == 0) {
-                        System.out.println("Ничего не найдено");
-                    } else {
-                        for (int i = 0; i < poisk.length; i++) {
-
-                            AddressBook.vvod (addressBook);
-                        }
+                        System.out.println("Ничего не найдено!");
                     }
                     break;
+                }
+                case 3: {
+                    System.out.println("Введите индекс для поиска:");
+                    int index = sc.nextInt();
+                    if (index < AddressBook.getCount(addressBook)) {
+                        AddressBook.get(addressBook, index);
+                    } else {
+                        System.out.println("Неверно указан индекс!");
                     }
-                case 3:
+                    break;
+                }
+                case 4:
                     AddressBook.print(addressBook);
                     break;
-                case 4: {
+                case 5: {
                     System.out.println("Введите индекс для удаления записи");
                     int index = sc.nextInt();
                     if (index < AddressBook.getCount(addressBook)) {
                         AddressBook.delete(addressBook, index);
-                        System.out.println("Запись успешно удалена");
+                        System.out.println("Запись успешно удалена!");
                     } else {
-                        System.out.println("Неверно указан индекс");
+                        System.out.println("Неверно указан индекс!");
                     }
+                    break;
                 }
-                case 5:
+                case 6:
                     System.out.println("Введите индекс для изменения записи");
                     String a = sc.nextLine();
-                    int index = Integer.parseInt (a);
+                    int index = Integer.parseInt(a);
                     if (index < AddressBook.getCount(addressBook)) {
                         System.out.println("Введите имя");
                         String name = sc.nextLine();
-                        System.out.println("Введите телефон");
+                        System.out.println("Введите телефон (в случае отстутствия введите \"нет\")");
                         String phone = sc.nextLine();
-                        System.out.println("Введите емайл");
+                        System.out.println("Введите емайл (в случае отстутствия введите \"нет\")");
                         String email = sc.nextLine();
-                        AddressBook.edit(addressBook, index, name, phone, email);
-                        System.out.println("Запись успешно изменена");
+                        System.out.println("Введите год рождения");
+                        String year = sc.nextLine();
+                        AddressBook.edit(addressBook, index, name, phone, email, year);
+                        System.out.println("Запись успешно изменена!");
                     } else {
-                        System.out.println("Неверно указан индекс");
+                        System.out.println("Неверно указан индекс!");
                     }
                     break;
-                case 6:
+                case 7:
                     System.out.println("Спасибо за использование!");
                     return;
                 default:
