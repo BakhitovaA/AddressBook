a8b781f598b22930cb73c2ca4d772b98a1986b8d
Анастасия <morina.anastasiya@gmail.com>
2014-10-22 22:55:02 +0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,24 +6,26 @@ public class AddressBook {
     private int count = 0;
 
     //Добавление записи в книгу
-    public static void add(AddressBook book, String name, String phone, String email) {
-        Address newAddress = new Address(name, phone, email);
+    public static void add(AddressBook book, String name, String phone, String email, String year) {
+        Address newAddress = new Address(name, phone, email, year);
         book.list[book.count] = newAddress;
         book.count++;
     }
 
     //Поиск записи по имени человека
     public static int[] find(AddressBook book, String name) {
-
+        // подсчитываем количество совпадений введенного имени и записей в книге
         int count = 0;
         for (int i = 0; i < book.count; i++) {
             if (!(book.list[i].name == null)) {
                 Address a = book.list[i];
                 if (a.name.equals(name)) {
-                     count++;
+                    count++;
                 }
             }
         }
+        // создаем новый массив размером, подсчитанным выше
+        // проверяем на совпадения, в случае совпадения выводим запись
         int[] poisk = new int[count];
         count = 0;
         for (int i = 0; i < book.count; i++) {
@@ -31,6 +33,7 @@ public class AddressBook {
                 Address a = book.list[i];
                 if (a.name.equals(name)) {
                     poisk[count] = i;
+                    System.out.println(i + "." + a.name + " / " + a.phone + " / " + a.email + " / " + a.year);
                     count++;
                 }
             }
@@ -38,42 +41,45 @@ public class AddressBook {
         return poisk;
     }
 
-    // Вывод всех записей на экран
-    public static void print (AddressBook book) {
-        System.out.println(" Имя  /   телефон  /  email");
-
-        for (int i=0; i<book.count; i++) {
-            Address address = book.list[i];
-            System.out.println(i + "." + address.name + " / " + address.phone + " / " + address.email);
-        }
+    //Поиск записи по индексу записи в книге
+    public static void get (AddressBook book, int index) {
+        Address b = book.list[index];
+        System.out.println(index + "." + b.name + " / " + b.phone + " / " + b.email+ " / " + b.year);
     }
 
-    public static void vvod (AddressBook book) {
-            int i=0;
-            Address address = book.list[i];
-            System.out.println(i + "." + address.name + " / " + address.phone + " / " + address.email);
 
+    // Вывод всех записей на экран
+    public static void print (AddressBook book) {
+        System.out.println(" Имя  /   Телефон  /  email / Год рождения");
+        for (int i=0; i<book.count; i++) {
+            Address address = book.list[i];
+            System.out.println(i + "." + address.name + " / " + address.phone + " / " + address.email+ " / " + address.year);
+        }
     }
 
     // Удаление записи из книги по индексу
     public static void delete(AddressBook book, int index) {
-
+        // присваиваем выбранной пользователем записи тип null
         book.list[index].name = null;
         book.list[index].phone = null;
         book.list[index].email = null;
-
+        book.list[index].year = null;
+        // перемещаем записи адресной книги влево на пустое место
         for (int i = index; i < book.count - 1; i++) {
             book.list[i].name = book.list[i + 1].name;
             book.list[i].phone = book.list[i + 1].phone;
             book.list[i].email = book.list[i + 1].email;
+            book.list[i].year = book.list[i + 1].year;
         }
         book.count --;
     }
 
-    public static void edit (AddressBook book, int index, String name, String phone, String email){
+    // Изменение записи в книге по индексу
+    public static void edit (AddressBook book, int index, String name, String phone, String email, String year){
         book.list [index].name=name;
         book.list [index].phone=phone;
         book.list [index].email=email;
+        book.list [index].year=year;
     }
 
     // Получение количества записей в книге
